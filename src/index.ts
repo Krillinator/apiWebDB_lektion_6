59b9f9cbb492095abba64f13f0359b57f494dfc9
Kristoffer Johansson <56152114+Krillinator@users.noreply.github.com>
2023-12-08 10:59:57 +0100

    Error handling for response

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { log } from "console"
 import express, { Request, Response } from "express"
 
 const app = express()
@@ -12,6 +13,19 @@ interface Student {
   grades: string[]
 }
 
+// Function Declaration
+const isStudent = (student: Student) => {
+  return (
+    typeof student === "object" &&
+    typeof student.name === "string" &&
+    typeof student.age === "number" &&
+    Array.isArray(student.grades) &&
+    student.grades.every((index) => {
+      return typeof index === "string"
+    })
+  )
+}
+
 // Endpoints
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, welcome to your Express API!")
@@ -23,13 +37,15 @@ app.get("/test", (req: Request, res: Response) => {
 })
 
 // Query Practice
-app.get("/student", (req: Request, res: Response) => {
-  const queryName: string = req.query.name as string
+app.get("/student", (req: Request<{}, {}, {}, Student>, res: Response) => {
+  const queryName = req.query.name
+  const queryAge = req.query.age
+  const queryGrades = req.query.grades
 
   const benny: Student = {
     name: queryName,
-    age: 17,
-    grades: ["A", "B", "C", "F", "D"],
+    age: queryAge,
+    grades: queryGrades,
   }
 
   res.json(benny)
@@ -37,14 +53,29 @@ app.get("/student", (req: Request, res: Response) => {
 
 // Query Post + Body - Practice
 app.post("/student", (req: Request, res: Response) => {
-  const student: Student = req.body
-
-  console.log(student)
-
-  res.status(201).json(student)
+  if (isStudent(req.body)) {
+    res.status(201).json(req.body)
+  } else {
+    res.status(400).send("Bad Request")
+  }
 })
 
 // Listen
 app.listen(PORT, () => {
+  const student = {
+    name: "Benny",
+    age: 100,
+    grades: ["abba", "banan"],
+  }
+
   console.log(`Server is running on http://localhost:${PORT}`)
+  console.log(typeof student === "object")
+  console.log(typeof student.name === "string")
+  console.log(typeof student.age === "number")
+  console.log(Array.isArray(student.grades))
+  console.log(
+    student.grades.every((value) => {
+      return typeof value === "string"
+    })
+  )
 })
